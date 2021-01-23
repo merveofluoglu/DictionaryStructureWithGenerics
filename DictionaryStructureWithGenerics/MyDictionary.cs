1c068104a2fcc327384d7752f66239b91ca2f9d2
ofluoglumerve <ofluoglumerve@gmail.com>
2021-01-23 15:43:58 +0300

    Add project files.

---
+++ DictionaryStructureWithGenerics/MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+// Here a dictionary was created with generics
+// Key and Value can be any type
+
+namespace DictionaryStructure
+{
+    class MyDictionary<T1, T2>
+    {
+        T1[] index; // For Dictionary structure.
+        T2[] element;
+
+        public MyDictionary()
+        {
+            index = new T1[0]; // Defining an allocation for arrays.
+            element = new T2[0];
+        }
+
+        public void Add(T1 key, T2 value)
+        {
+            T1[] tempIndex = index;
+            T2[] tempElement = element;
+
+            index = new T1[index.Length + 1];
+            element = new T2[element.Length + 1];
+
+            for (int i = 0; i < tempIndex.Length; i++)
+            {
+                index[i] = tempIndex[i];
+                element[i] = tempElement[i];
+            }
+
+            index[index.Length - 1] = key;
+            element[element.Length - 1] = value;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < index.Length; i++)
+            {
+                Console.WriteLine("Key: " + index[i]);
+                Console.WriteLine("Value: " + element[i]);
+                Console.WriteLine("************************************");
+            }
+        }
+    }
+}
---
+++ DictionaryStructureWithGenerics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/*
+ This program is a dictionary program that 
+ defines the dictionary structure and includes simple methods
+
+ Program takes key and value inputs from the user
+ and adds them into the dictionary that i made and prints it 
+ if the method is called.
+ */
+
+namespace DictionaryStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dict1 = new MyDictionary<int, string>();
+
+            Console.WriteLine("Please enter you key and value respesctively: ");
+            Console.WriteLine("Press '0' to exit.");
+
+            while (true)
+            {
+                int key;
+                string value;
+                key = Convert.ToInt32(Console.ReadLine());
+                if (key == 0)
+                {
+                    break;
+                }
+                value = Console.ReadLine();
+                dict1.Add(key, value);
+            }
+
+            dict1.Print();
+        }
+    }
+}
